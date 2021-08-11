0a92d6e092d76c35aed5d5d0db3e6278c19cdc98
codegangsk <kjhj0604@gmail.com>
2021-08-11 19:49:18 +0900

    create Card.swift/Cards.swift

---
+++ PickOneCard/Models/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  PickOneCard
+//
+//  Created by Sophie Kim on 2021/08/11.
+//
+
+import Foundation
---
+++ PickOneCard/Models/Cards.swift
@@ -0,0 +1,8 @@
+//
+//  Cards.swift
+//  PickOneCard
+//
+//  Created by Sophie Kim on 2021/08/11.
+//
+
+import Foundation
