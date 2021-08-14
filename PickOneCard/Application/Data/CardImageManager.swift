0228f2f31fc4d40e034576c508fd8c9692de706a
codegangsk <kjhj0604@gmail.com>
2021-08-14 20:44:24 +0900

    create CardImageManager struct

---
+++ PickOneCard/Application/Data/CardImageManager.swift
@@ -0,0 +1,13 @@
+//
+//  CardImageManager.swift
+//  PickOneCard
+//
+//  Created by Sophie Kim on 2021/08/14.
+//
+
+import Foundation
+import UIKit
+import Kingfisher
+
+struct CardImageManager {
+}
