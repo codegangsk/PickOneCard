f47e9e748325bfb7b2176a8513db4a8fab84e2db
codegangsk <kjhj0604@gmail.com>
2021-08-25 14:52:13 +0900

    move fetchCards function to CardRepository

--- PickOneCard/Application/Data/CardRepository.swift
+++ PickOneCard/Application/Data/CardRepository.swift
@@ -9,10 +9,29 @@ import Foundation
 import UIKit
 import Alamofire
 
-/*struct CardRepository {
-    var cardRepository : [Cards]
+class CardRepository {
+    var cardRepository : [Card] = []
     
     init() {
+        fetchCards()
     }
 }
-*/
+
+extension CardRepository {
+    func fetchCards() {
+        API.shared.call(url: "https://rws-cards-api.herokuapp.com/api/v1/cards", for: CardsResponse.self) {
+             [weak self] response in
+             guard let self = self else { return }
+
+             switch response {
+             case .success(let cardsResponse):
+                 let cards = cardsResponse.cards
+                 self.cardRepository = cards!
+             case .failure(let error):
+                 break
+             }
+         }
+     }
+}
+
+
